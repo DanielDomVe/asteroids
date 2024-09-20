f32379a9b5fc3ac5ae46fdbf7bf4b7a2017127b1
DanielDomVe <daniel.dominguez.veiga@gmail.com>
2024-09-20 17:11:42 +0200

    Asteroids spawning and moving implemented

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import pygame # type: ignore
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # GROUPS
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -23,12 +38,16 @@ def main():
         
         # game logic
 
-        player.update(dt)
+        for object in updatable:
+            object.update(dt)
 
 
         # rendering
         screen.fill(000)
-        player.draw(screen) 
+        
+        for object in drawable:
+            object.draw(screen) 
+        
         pygame.display.flip()
 
         # 60 FPS limit
