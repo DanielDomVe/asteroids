e18d1d630481ed891dbbfb1691f188e8f60b003f
DanielDomVe <daniel.dominguez.veiga@gmail.com>
2024-09-20 15:22:38 +0200

    Added player triangle and circle class to the game

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    t = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,9 +18,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill(000)
-
+        player.draw(screen) 
         pygame.display.flip()
+        dt = t.tick(60)/1000
+
 
 
 
